50bc4981b54eb52e46532a0f181745a2b885f53b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-21 20:29:00 -0500

    Added a clarification comment

--- play.js
+++ play.js
@@ -12,7 +12,7 @@ const connect = function () {
   });
 
   conn.on('data', (data) => {
-    console.log(data.toString()); // Will show data written in Lighthouse Labs repository ("you ded cuz you idled");
+    console.log(data.toString()); // Will show data written in Lighthouse Labs repository once you disconnect ("you ded cuz you idled");
     conn.end();
   });
 
