3623d2dd307c29d96915b7d837f0dda50f45e573
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-22 13:40:47 -0500

    Stdin setup

--- play.js
+++ play.js
@@ -1,1 +1,18 @@
-const connect = require("./client.js");
+const { connect } = require("./client.js");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if (key === '\u0003') {  // \u0003 maps to ctrl+c input
+    process.exit();
+  }
+};
