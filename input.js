2d2a1cd9352cd293b07448009a8c6fcabcbac278
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-22 17:56:12 -0500

    Adding connection and keyMapping to give player movement abilities

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection; // Stores the active TCP connection object
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,10 +9,18 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+const keyMapping = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right"
+}
 
 const handleUserInput = function (key) {
   if (key === '\u0003') {  // \u0003 maps to ctrl+c input
     process.exit();
+  } if (key in keyMapping) {
+    connection.write(keyMapping[key]);
   }
 };
 
