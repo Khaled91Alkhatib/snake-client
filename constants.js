e0503482490ce8b88d56359242b1d9a1164994be
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-22 19:44:11 -0500

    Moved constant values from input.js and client.js to here and exported them for a cleaner code overall

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = "localhost";
+const PORT = 50541;
+const keyMapping = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  p: "Say: GOTCHA",
+  o: "Say: try again!"
+}
+
+
+module.exports = {
+  IP,
+  PORT,
+  keyMapping
+};
